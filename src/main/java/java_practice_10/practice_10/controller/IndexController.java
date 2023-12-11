e77006f688fb366fc9fff1100143e588a1112627
Dmitrii Toropov <dimator1973@gmail.com>
2023-12-11 18:39:28 +0500

    initial

---
+++ src/main/java/java_practice_10/practice_10/controller/IndexController.java
@@ -0,0 +1,14 @@
+package java_practice_10.practice_10.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/test/java/java_practice_10/practice_10/Practice10ApplicationTests.java
@@ -0,0 +1,13 @@
+package java_practice_10.practice_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
